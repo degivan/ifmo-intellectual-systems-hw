78109b418ff3d087cd42735eef8e4cbfc0963b66
vanyadeg <id1997@yandex.ru>
2017-09-14 01:33:25 +0300

    LAB-1: changing dataset file

--- KNearestNeighbour/src/main/kotlin/degtiarenko/ml/KNearestNeighbour.kt
+++ KNearestNeighbour/src/main/kotlin/degtiarenko/ml/KNearestNeighbour.kt
@@ -1,17 +1,18 @@
 package degtiarenko.ml
 
 
-fun main(args : Array<String>) {
-    val lines = ClassLoader.getSystemClassLoader().getResource("trainingdata.txt")
-            .readText().split("\n")
+fun main(args: Array<String>) {
+    var lines = ClassLoader.getSystemClassLoader().getResource("chips.txt")
+            .readText().split("\n").toMutableList()
     var datasetSize = lines[0].toInt()
+    lines.removeAt(0)
 
     println(kernelFun(2.0))
     println(kernelFun(0.5))
     println(kernelFun(0.3))
 }
 
-fun kernelFun(x : Double) : Double {
+fun kernelFun(x: Double): Double {
     return if (Math.abs(x) > 1) {
         0.0
     } else {
