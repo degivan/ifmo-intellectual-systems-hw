4561bef844815b6b974b7db7227e322a65b06f23
vanyadeg <id1997@yandex.ru>
2017-09-14 01:14:33 +0300

    Project renamed

---
+++ KNearestNeighbour/src/main/kotlin/degtiarenko/ml/KNearestNeighbour.kt
@@ -0,0 +1,20 @@
+package degtiarenko.ml
+
+
+fun main(args : Array<String>) {
+    val lines = ClassLoader.getSystemClassLoader().getResource("trainingdata.txt")
+            .readText().split("\n")
+    var datasetSize = lines[0].toInt()
+
+    println(kernelFun(2.0))
+    println(kernelFun(0.5))
+    println(kernelFun(0.3))
+}
+
+fun kernelFun(x : Double) : Double {
+    return if (Math.abs(x) > 1) {
+        0.0
+    } else {
+        1 - Math.abs(x)
+    }
+}
