6c02b7b5569ebdd0762a8444559c49a96439a0c5
degivan <id1997@yandex.ru>
2017-10-25 12:30:38 +0300

    LAB-3: the dumbest version of naive bayes classifier

--- naive-bayes/itmo/lab/spam_naive_bayes.py
+++ naive-bayes/itmo/lab/spam_naive_bayes.py
@@ -1,16 +1,69 @@
 import os
 
+from math import log
+
 
 def train(train_data):
     total = 0
-    num_spam = 0
+    mail_spam = 0
+    subj_words_count = [0, 0]
+    text_words_count = [0, 0]
+    subject_words = [{},{}]
+    text_words = [{}, {}]
     for email in train_data:
-        if email.label == 'spmsg':
-            num_spam += 1
+        positive = email.label == 'SPAM'
+        mail_spam += 1 * positive
+        for word in email.subject:
+            subject_words[positive][word] = subject_words[positive].get(word, 0) + 1
+            subj_words_count[positive] += 1
+        for word in email.text:
+            text_words[positive][word] = text_words[positive].get(word, 0) + 1
+            text_words_count[positive] += 1
         total += 1
-    p_spam = num_spam / total
+    p_spam = mail_spam / float(total)
     p_ham = 1 - p_spam
-    return p_spam, p_ham
+    return Classifier(p_spam, p_ham, subj_words_count, subject_words, text_words_count, text_words)
+
+
+class Classifier(object):
+    def __init__(self, p_spam, p_ham, subj_wc, subj_w, text_wc, text_w):
+        self.p_spam = p_spam
+        self.p_ham = p_ham
+        self.subj_wc = subj_wc
+        self.subj_w = subj_w
+        self.text_wc = text_wc
+        self.text_w = text_w
+
+    def classify(self, email):
+        is_spam = self.cond_email(email, True)
+        is_ham = self.cond_email(email, False)
+        if is_spam < is_ham:
+            return 'SPAM'
+        else:
+            return 'HAM'
+
+    def cond_email(self, email, spam):
+        sum = 0.0
+        if spam:
+            sum -= log(self.p_spam)
+        else:
+            sum -= log(self.p_ham)
+        for word in email.subject:
+            sum -= log(self.cond_subj_word(word, spam), 10 ** (-7))
+        for word in email.text:
+            sum -= log(self.cond_text_word(word, spam), 10 ** (-7))
+
+    def cond_subj_word(self, word, spam):
+        word_count = self.subj_w[spam].get(word, 0)
+        if word_count == 0:
+            word_count = 1 + 1000 * spam
+        return word_count / float(self.subj_wc[spam])
+
+    def cond_text_word(self, word, spam):
+        word_count = self.text_w[spam].get(word, 0)
+        if word_count == 0:
+            word_count = 1 + 1000 * spam
+        return word_count / float(self.text_wc[spam])
 
 
 class Email(object):
@@ -32,7 +85,7 @@ def create_email_obj(title, content):
 
 
 if __name__ == '__main__':
-    train_data = [[] for i in range(10)]
+    data = [[] for i in range(10)]
     for i in range(1, 11):
         path = '../../data/part' + str(i)
         for f in os.listdir(path):
@@ -40,5 +93,7 @@ if __name__ == '__main__':
             content = opened_f.read()
             title = f.title()
             opened_f.close()
-            train_data[i - 1].append(create_email_obj(title, content))
-    
+            data[i - 1].append(create_email_obj(title, content))
+    classifier = train(data[0])
+    print (classifier.classify(data[1][0]))
+    print (data[1][0].label)
